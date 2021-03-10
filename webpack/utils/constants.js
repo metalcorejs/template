cc3109df0d4ed67827a5d5abf86164752edf3bad
Tony Kark <tonyfullstackkark10@gmail.com>
2021-03-11 00:30:01 +0300

    constants for roots

---
+++ webpack/utils/constants.js
@@ -0,0 +1,13 @@
+// Core
+import { path as ROOT_DIR } from "app-root-path";
+import { resolve } from "path";
+
+// Constants
+export { ROOT_DIR };
+export const SRC_DIR = resolve(ROOT_DIR, "src");
+export const DIST_DIR = resolve(ROOT_DIR, "dist");
+export const BUILD_DIR = resolve(ROOT_DIR, "build");
+export const HTML_DIR = resolve(ROOT_DIR, "src/html");
+export const ASSETS_DIR = resolve(ROOT_DIR, "src/assets");
+export const HOST = "localhost";
+export const PORT = process.env.PORT || 8080;
