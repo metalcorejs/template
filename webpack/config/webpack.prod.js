a5632368962204ad32031d5d1664a19cec701dc3
Tony Kark <tonyfullstackkark10@gmail.com>
2021-03-11 02:41:14 +0300

    bundle analyzer added

--- webpack/config/webpack.prod.js
+++ webpack/config/webpack.prod.js
@@ -1,5 +1,6 @@
 // Core
 import { merge } from "webpack-merge";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 // Constants
 import { BUILD_DIR } from "../utils/constants";
@@ -7,25 +8,33 @@ import { BUILD_DIR } from "../utils/constants";
 // Common Config
 import getCommonConfig from "./webpack.common";
 
+// Modules
+import * as modules from "../modules";
+
 // Prod Config
 export default () =>
-  merge(getCommonConfig(), {
-    mode: "production",
-    devtool: false,
-    output: {
-      filename: "bundle.[contenthash].js",
-      path: BUILD_DIR,
-    },
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          vendor: {
-            name: "vendors",
-            test: /node_modules/,
-            chunks: "all",
-            enforce: true,
+  merge(
+    getCommonConfig(),
+    {
+      mode: "production",
+      devtool: false,
+      output: {
+        filename: "bundle.[contenthash].js",
+        path: BUILD_DIR,
+      },
+      plugins: [new BundleAnalyzerPlugin({ analyzerMode: "disabled" })],
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              name: "vendors",
+              test: /node_modules/,
+              chunks: "all",
+              enforce: true,
+            },
           },
         },
       },
     },
-  });
+    modules.cleanDir()
+  );
