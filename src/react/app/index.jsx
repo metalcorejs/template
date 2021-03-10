cdd59eb20f17e9a5b4168d44f48b7c51dd2461da
Tony Kark <tonyfullstackkark10@gmail.com>
2021-03-11 01:59:16 +0300

    rename tsx files and add images

---
+++ src/react/app/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import img1 from "../../static/images/1.jpg";
+import img1jpg from "../../static/images/1.webp";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <picture>
+        <source srcSet={img1} />
+        <img loading="lazy" src={img1jpg} alt="running man" className="img" />
+      </picture>
+    </>
+  );
+}
+
+export default App;
