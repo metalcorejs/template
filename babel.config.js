69356b1d640ac79f835968229186ae92b76dc12e
Tony Kark <tonyfullstackkark10@gmail.com>
2021-03-11 01:20:19 +0300

    babel config and browserslist

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+export default (api) => {
+  // Cache
+  api.cache.never();
+
+  return {
+    presets: [
+      "@babel/react",
+      [
+        "@babel/env",
+        {
+          debug: false,
+          spec: true,
+          loose: true,
+          modules: false,
+        },
+      ],
+    ],
+  };
+};
