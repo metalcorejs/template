9482eb627fa877c08e9391b00c316de96238645c
Tony Kark <tonyfullstackkark10@gmail.com>
2021-03-14 01:38:49 +0300

    calendar update

--- src/react/app/ui/cards/date_picker_form/date_picker/calendar/index.tsx
+++ src/react/app/ui/cards/date_picker_form/date_picker/calendar/index.tsx
@@ -1,12 +1,23 @@
 import * as React from "react";
 
 export const Calendar = () => {
+  const weekDays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
   return (
     <div className="calendar">
       <div className="calendar__slider">
         <button className="calendar__btn calendar__btn_back"></button>
         <time className="calendar__date">August 2021</time>
         <button className="calendar__btn calendar__btn_forward"></button>
+      </div>
+      <div className="calendar__week-days">
+        {weekDays.map((day) => (
+          <span className="calendar__week-day">{day}</span>
+        ))}
+      </div>
+      <div className="calendar__month-days">
+        {[...new Array(42)].fill(null).map((_, i) => (
+          <span className="calendar__month-day">{i + 1}</span>
+        ))}
       </div>
     </div>
   );
