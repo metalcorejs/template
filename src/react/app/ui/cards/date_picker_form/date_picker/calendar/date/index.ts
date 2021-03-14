2771e05c41e3b620b8c798a6971a767ef24c6011
Tony Kark <tonyfullstackkark10@gmail.com>
2021-03-14 13:47:47 +0300

    calendar update

---
+++ src/react/app/ui/cards/date_picker_form/date_picker/calendar/date/index.ts
@@ -0,0 +1,53 @@
+const months = [
+  "Январь",
+  "Февраль",
+  "Март",
+  "Апрель",
+  "Май",
+  "Июнь",
+  "Июль",
+  "Август",
+  "Сентябрь",
+  "Октябрь",
+  "Ноябрь",
+  "Декабрь",
+];
+
+const getDays = (date: Date) =>
+  [...new Array(new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate())]
+    .fill(null)
+    .map((_, i) => i + 1);
+
+const getPrevDays = (date: Date) => {
+  const lastDayPrevMonth = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDay();
+  const lastDayDatePrevMonth = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  if (lastDayPrevMonth === 0) return [];
+  else {
+    const prevDays = 7 - lastDayPrevMonth;
+    return [...new Array(prevDays)]
+      .fill(null)
+      .map((_, i) => lastDayDatePrevMonth - i)
+      .reverse();
+  }
+};
+
+export const setDate = (date: Date = new Date()) => {
+  const year = date.getFullYear();
+  const month = months[date.getMonth()];
+  const monthNumber = date.getMonth();
+  const days = getDays(date);
+  const prevDays = getPrevDays(date);
+  const nextDays = [...new Array(42 - days.length - prevDays.length)]
+    .fill(null)
+    .map((_, i) => i + 1);
+  return { year, month, monthNumber, days, prevDays, nextDays };
+};
--- src/react/app/ui/cards/date_picker_form/date_picker/calendar/index.tsx
+++ src/react/app/ui/cards/date_picker_form/date_picker/calendar/index.tsx
@@ -1,13 +1,35 @@
-import * as React from "react";
+import React, { useState } from "react";
+import { setDate } from "./date";
 
 export const Calendar = () => {
+  const [calendar, setCalendar] = useState(setDate);
+  const { year, monthNumber, month, days, prevDays, nextDays } = calendar;
+  console.log(year, monthNumber, month, days, prevDays, nextDays);
   const weekDays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+
+  const setNextMonth = () => {
+    const newDate = new Date(year, monthNumber + 1);
+    setCalendar(setDate(newDate));
+  };
+
+  const setPrevMonth = () => {
+    const newDate = new Date(year, monthNumber - 1);
+    setCalendar(setDate(newDate));
+  };
   return (
     <div className="calendar">
       <div className="calendar__slider">
-        <button className="calendar__btn calendar__btn_back"></button>
-        <time className="calendar__date">August 2021</time>
-        <button className="calendar__btn calendar__btn_forward"></button>
+        <button
+          className="calendar__btn calendar__btn_back"
+          type="button"
+          onClick={setPrevMonth}
+        ></button>
+        <time className="calendar__date">{`${month} ${year}`}</time>
+        <button
+          className="calendar__btn calendar__btn_forward"
+          onClick={setNextMonth}
+          type="button"
+        ></button>
       </div>
       <div className="calendar__week-days">
         {weekDays.map((day) => (
@@ -15,8 +37,18 @@ export const Calendar = () => {
         ))}
       </div>
       <div className="calendar__month-days">
-        {[...new Array(42)].fill(null).map((_, i) => (
-          <span className="calendar__month-day">{i + 1}</span>
+        {prevDays.map((day, i) => (
+          <span className="calendar__month-day calendar__month-day_prev">
+            {day}
+          </span>
+        ))}
+        {days.map((day, i) => (
+          <span className="calendar__month-day">{day}</span>
+        ))}
+        {nextDays.map((day, i) => (
+          <span className="calendar__month-day calendar__month-day_next">
+            {day}
+          </span>
         ))}
       </div>
     </div>
