348cb6ed0e22fb0913c57aad445eab2dc309142f
Tony Kark <tonyfullstackkark10@gmail.com>
2021-03-15 22:14:18 +0300

    dropdown logic

--- src/react/app/ui/dropdown/index.tsx
+++ src/react/app/ui/dropdown/index.tsx
@@ -1,21 +1,85 @@
-import * as React from "react";
+import React, { useState, MouseEvent } from "react";
 
 export const Dropdown = () => {
-  const options = ["взрослые", "дети", "младенцы"];
+  const options = [
+    { name: "взрослые", amount: 0 },
+    { name: "дети", amount: 0 },
+    { name: "младенцы", amount: 0 },
+  ];
+  const [guests, setGuests] = useState(options);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const optionsJsx = options.map((option) => (
+  const incGuests = (e: MouseEvent) => {
+    const target = e.target as HTMLButtonElement;
+    console.log(target.dataset);
+    setGuests((prev) => {
+      let newState = [...prev];
+      newState = newState.map(({ name, amount }) => {
+        if (name === target.dataset.option) return { name, amount: amount + 1 };
+        return { name, amount };
+      });
+      return newState;
+    });
+  };
+
+  const dropdownHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const decGuests = (e: MouseEvent) => {
+    const target = e.target as HTMLButtonElement;
+    console.log(target.dataset.option === "взрослые");
+    const isNull = guests.find(
+      ({ name, amount }) => target.dataset.option === name && amount > 0
+    );
+    console.log(isNull);
+    if (!isNull) return;
+    setGuests((prev) => {
+      let newState = [...prev];
+      newState = newState.map(({ name, amount }) => {
+        if (name === target.dataset.option) return { name, amount: amount - 1 };
+        return { name, amount };
+      });
+      return newState;
+    });
+  };
+
+  const optionsJsx = guests.map(({ name, amount }) => (
     <div className="dropdown__option">
-      <p className="dropdown__option-title">{option}</p>
-      <button className="dropdown__option-btn">-</button>
-      <span className="dropdown__option-amount">0</span>
-      <button className="dropdown__option-btn">+</button>
+      <p className="dropdown__option-title">{name}</p>
+      <button
+        className={`dropdown__option-btn ${
+          !amount && "dropdown__option-btn_empty"
+        }`}
+        data-option={name}
+        onClick={decGuests}
+      >
+        -
+      </button>
+      <span className="dropdown__option-amount">{amount}</span>
+      <button
+        className="dropdown__option-btn"
+        data-option={name}
+        onClick={incGuests}
+      >
+        +
+      </button>
     </div>
   ));
 
   return (
-    <div className="dropdown">
-      <div className="dropdown__select">Гости</div>
-      <div className="dropdown__options">{optionsJsx}</div>
+    <div className={`dropdown`}>
+      <div
+        className={`dropdown__select ${isOpen ? "dropdown__select_open" : ""}`}
+        onClick={dropdownHandler}
+      >
+        Гости
+      </div>
+      <div
+        className={`dropdown__options ${isOpen && "dropdown__options_open"}`}
+      >
+        {optionsJsx}
+      </div>
     </div>
   );
 };
